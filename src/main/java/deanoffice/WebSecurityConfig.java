0665c20d14647d20f7d85430f97c143f7a875636
AdamStan <adam15stan@gmail.com>
2018-07-29 19:31:26 +0200

    rozgrzebane, nie dziala

--- src/main/java/deanoffice/WebSecurityConfig.java
+++ src/main/java/deanoffice/WebSecurityConfig.java
@@ -1,18 +1,45 @@
 package deanoffice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled"
+                        + " from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority "
+                        + "from authorities where username=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+                .and()
+                .httpBasic(); // Authenticate users with HTTP basic authentication
+    }*/
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -20,14 +47,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+                .and()
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll();
+                .permitAll()
+                .and();
     }
-
+    /*
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -40,4 +70,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         return new InMemoryUserDetailsManager(user);
     }
+    */
 }
