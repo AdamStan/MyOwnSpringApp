ab3549d057671f26d6cbf1f7fc21a85e2aed3da0
AdamStan <adam15stan@gmail.com>
2018-08-16 20:20:09 +0200

    I've started, nothing works now

--- src/main/java/deanoffice/controllers/AdminController.java
+++ src/main/java/deanoffice/controllers/AdminController.java
@@ -1,14 +1,73 @@
 package deanoffice.controllers;
 
+import deanoffice.entities.*;
+import deanoffice.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.jws.WebParam;
+
 @Controller
 public class AdminController {
-    @RequestMapping(value = "/allmarks", method = RequestMethod.GET)
+    @Autowired
+    private MarkRepository markRepository;
+    @Autowired
+    private SubjectRepository subjectRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private TutorRepository tutorRepository;
+    @Autowired
+    private FacultyRepository facultyRepository;
+
+    @RequestMapping(value = "/admin/allmarks", method = RequestMethod.GET)
     public ModelAndView marks(){
-        return new ModelAndView("/admin/marks.html");
+        Iterable<Mark> marks = markRepository.findAll();
+        ModelAndView model = new ModelAndView("/admin/marks.html");
+        model.addObject("marks", marks);
+        return model;
     }
+    @RequestMapping(value = "/admin/allsubjects", method = RequestMethod.GET)
+    public ModelAndView subjects(){
+        Iterable<Subject> subjects = subjectRepository.findAll();
+        ModelAndView model = new ModelAndView("/admin/subjects.html");
+        model.addObject("subjects", subjects);
+        return model;
+    }
+
+    @RequestMapping(value = "/admin/allstudents", method = RequestMethod.GET)
+    public ModelAndView students(){
+        Iterable<Student> students = studentRepository.findAll();
+        return new ModelAndView("/admin/students.html");
+    }
+
+    @RequestMapping(value = "/admin/alltutors", method = RequestMethod.GET)
+    public ModelAndView tutors(){
+        Iterable<Tutor> tutors = tutorRepository.findAll();
+        return new ModelAndView("/admin/tutors.html");
+    }
+
+    @RequestMapping(value = "/admin/allfaculties", method = RequestMethod.GET)
+    public ModelAndView faculties(){
+        Iterable<Faculty> faculties = facultyRepository.findAll();
+        ModelAndView model = new ModelAndView("/admin/faculties.html");
+        model.addObject("faculties", faculties);
+        return model;
+    }
+
+    @RequestMapping(value = "/admin/allusers", method = RequestMethod.GET)
+    public ModelAndView users(){
+        // ?
+        return new ModelAndView("/admin/users.html");
+    }
+
+    @RequestMapping(value = "/admin/auths", method = RequestMethod.GET)
+    public ModelAndView authorities(){
+        // ?
+        return new ModelAndView("/admin/auths.html");
+    }
+
 }
