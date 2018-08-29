e603665262aef80f9eed8275a9ecd5707b6438d8
AdamStan <adam15stan@gmail.com>
2018-08-29 21:20:03 +0200

    functionalities for student work fine

--- src/main/java/deanoffice/controllers/TutorController.java
+++ src/main/java/deanoffice/controllers/TutorController.java
@@ -21,7 +21,7 @@ public class TutorController {
 
     @RequestMapping(value = "/tutor/marks", method = RequestMethod.GET)
     public ModelAndView marks(HttpServletRequest request){
-        ModelAndView model = new ModelAndView("/student/studentsMarks.html");
+        ModelAndView model = new ModelAndView("/tutor/marks.html");
         String username = request.getRemoteUser();
         Tutor tutor = tutorRepository.findByUsername(username);
         Set<Subject> subjects = tutor.getSubjects();
@@ -39,7 +39,7 @@ public class TutorController {
 
     @RequestMapping(value = "/tutor/subjects", method = RequestMethod.GET)
     public ModelAndView subjects(HttpServletRequest request){
-        ModelAndView model = new ModelAndView("/student/studentsSubjects.html");
+        ModelAndView model = new ModelAndView("/tutor/subjects.html");
         String username = request.getRemoteUser();
         Tutor tutor = tutorRepository.findByUsername(username);
         model.addObject("subjects", tutor.getSubjects());
@@ -47,8 +47,30 @@ public class TutorController {
     }
 
     @RequestMapping(value = "/tutor/marks/add", method = RequestMethod.GET)
-
+    public ModelAndView addMark(HttpServletRequest request){
+        ModelAndView model = new ModelAndView();
+        return model;
+    }
     @RequestMapping(value = "/tutor/marks/addconfirm", method = RequestMethod.POST)
+    public ModelAndView addMarkConfirm(HttpServletRequest request){
+        ModelAndView model = new ModelAndView();
+        return model;
+    }
 
     @RequestMapping(value = "/tutor/marks/delete", method = RequestMethod.POST)
+    public ModelAndView deletMark(HttpServletRequest request){
+        String markid = request.getParameter("");
+        ModelAndView model = new ModelAndView();
+        return model;
+    }
+    @RequestMapping(value = "/tutor/marks/edit", method = RequestMethod.GET)
+    public ModelAndView editMark(HttpServletRequest request){
+        ModelAndView model = new ModelAndView();
+        return model;
+    }
+    @RequestMapping(value = "/tutor/marks/editconfirm", method = RequestMethod.POST)
+    public ModelAndView editMarkConfirm(HttpServletRequest request){
+        ModelAndView model = new ModelAndView();
+        return model;
+    }
 }
